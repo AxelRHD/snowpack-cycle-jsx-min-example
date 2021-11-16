7eb8aa7283f3de9ec415fd6b6418c5b72b051f10
Axel Rudolf <axelr@ubuntu-development.fritz.box>
2021-11-17 00:10:02 +0100

    init repo

---
+++ snowpack.config.js
@@ -0,0 +1,23 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/reference/configuration
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    /* ... */
+  },
+  plugins: [
+    /* ... */
+  ],
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    jsxFactory: 'jsx',
+    jsxFragment: 'fragment',
+    jsxInject: "import {jsx, fragment} from 'vista-jsx'"
+  },
+};
